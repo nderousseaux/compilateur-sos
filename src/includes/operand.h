1db2bed2aec6980be0c3657d73b3666700aa50db
Arnaud Filippi <arnaud.filippi@etu.unistra.fr>
2022-12-23 15:55:44 +0100

    création de la branche

--- src/includes/operand.h
+++ src/includes/operand.h
@@ -6,14 +6,16 @@
 #define SRC_INCLUDES_OPERAND_H_
 
 // Définit le type d'un operateur
-typedef enum Operator {
+typedef enum Operator
+{
     OP_EXIT,
     OP_ASSIGN,
     OP_ECHO,
 } Operator;
 
 // Définit le type d'une operande
-typedef enum Type_operand {
+typedef enum Type_operand
+{
     ID,
     VAR,
     INTEGER,
@@ -21,22 +23,23 @@ typedef enum Type_operand {
 } Type_operand;
 
 // Définit un operande
-typedef struct Operand {
-    Type_operand type;          // Type de l'operande
-    char * value;               // Valeur de l'operande (si c'est une variable)
-    int integer_value;          // Valeur de l'operande (si c'est une constante)
+typedef struct Operand
+{
+    Type_operand type; // Type de l'operande
+    char *value;       // Valeur de l'operande (si c'est une variable)
+    int integer_value; // Valeur de l'operande (si c'est une constante)
 } Operand;
 
 /* Crée une opérande de type var */
-Operand var(char * value);
+Operand var(char *value);
 
 /* Crée une opérande de type int */
 Operand integer(int value);
 
 /* Crée une opérande de type id */
-Operand id(char * name);
+Operand id(char *name);
 
 /* Crée une opérande de type empty (pas d'operande) */
 Operand empty();
 
-#endif  // SRC_INCLUDES_OPERAND_H_
+#endif // SRC_INCLUDES_OPERAND_H_
